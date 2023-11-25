f906aed512ae337cdbba257ba93b36124d638103
Daniel Rochetti <daniel.rochetti@gmail.com>
2023-11-25 09:24:24 -0800

    feat: use fal realtime client

--- src/App.js
+++ src/App.js
@@ -1,70 +1,22 @@
-import React, { useRef, useEffect, useCallback, useMemo, useState } from 'react';
-import { debounce } from 'lodash';
+import * as fal from '@fal-ai/serverless-client';
+import { useEffect, useRef, useState } from 'react';
 
-const DEBOUNCE_TIME = 0.1; // Adjust as needed
-const URL = "wss://110602490-lcm-sd15-i2i.gateway.alpha.fal.ai/ws";
+const APP_ID = '110602490-lcm-plexed-sd15-i2i';
+const PROMPT = 'A moon in a starry night sky';
 const CIRCLE_RADIUS = 80; // Radius of the circle
 
 function App() {
   const canvasRef = useRef(null);
-  const webSocketRef = useRef(null);
-  const isReconnecting = useRef(false);
   const [receivedImage, setReceivedImage] = useState(null);
 
-  const connect = useCallback(() => {
-    webSocketRef.current = new WebSocket(URL);
-    webSocketRef.current.onopen = () => {
-      console.log('WebSocket Open');
-    };
-
-    webSocketRef.current.onclose = () => {
-      console.log('WebSocket Close');
-    };
-
-    webSocketRef.current.onerror = (error) => {
-      console.error("WebSocket Error:", error);
-    };
-
-    webSocketRef.current.onmessage = (message) => {
-      try {
-        const data = JSON.parse(message.data);
-        console.log("WebSocket Message:", data);
-        if (data.images && data.images.length > 0) {
-          setReceivedImage(data.images[0].url);
-        }
-      } catch (e) {
-        console.error("Error parsing the WebSocket response:", e);
+  const { send: sendCurrentData } = fal.realtime.connect(APP_ID, {
+    connectionKey: 'fal-realtime-example',
+    onResult: (result) => {
+      if (result.images && result.images[0]) {
+        setReceivedImage(result.images[0].url);
       }
-    };
-  }, []);
-
-  const disconnect = useCallback(() => {
-    webSocketRef.current?.close();
-  }, []);
-
-  const sendMessage = useCallback(async (message) => {
-    if (!isReconnecting.current && webSocketRef.current?.readyState !== WebSocket.OPEN) {
-      isReconnecting.current = true;
-      connect();
-    }
-
-    if (isReconnecting.current && webSocketRef.current?.readyState !== WebSocket.OPEN) {
-      await new Promise((resolve) => {
-        const checkConnection = setInterval(() => {
-          if (webSocketRef.current?.readyState === WebSocket.OPEN) {
-            clearInterval(checkConnection);
-            resolve();
-          }
-        }, 100);
-      });
-      isReconnecting.current = false;
-    }
-    webSocketRef.current?.send(message);
-  }, [connect]);
-
-  const sendCurrentData = useMemo(() => {
-    return debounce(sendMessage, DEBOUNCE_TIME);
-  }, [sendMessage]);
+    },
+  });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -86,16 +38,15 @@ function App() {
       const x = event.clientX - canvas.offsetLeft;
       const y = event.clientY - canvas.offsetTop;
       drawCircle(x, y);
-      
+
       const imageData = canvas.toDataURL('image/png');
-      const req = {
-        prompt: 'A moon in a starry night sky',
-        seed: Math.floor(Math.random() * 100),
+      sendCurrentData({
+        prompt: PROMPT,
+        seed: 11252023,
         image_url: imageData,
         sync_mode: true,
         strength: 0.5,
-      };
-      sendCurrentData(JSON.stringify(req));
+      });
     };
 
     canvas.addEventListener('mousemove', handleMouseMove);
@@ -103,12 +54,21 @@ function App() {
     return () => {
       canvas.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [connect, disconnect, sendCurrentData]);
+  }, []);
 
   return (
     <div className="App">
-      <canvas ref={canvasRef} style={{border: "1px solid black"}} width={700} height={500} />
-      {receivedImage && <img style={{border: "1px solid black"}} src={receivedImage} alt="Received from server" />}
+      <canvas
+        ref={canvasRef}
+        style={{ border: '1px solid black' }}
+        width={512}
+        height={512}
+      />
+      <div style={{ border: '1px solid black', width: 512, height: 512 }}>
+        {receivedImage && (
+          <img src={receivedImage} alt="Received from server" />
+        )}
+      </div>
     </div>
   );
 }
